165ffad5ff7fcb5b9c975ed2178013b17f0280aa
emersonline2007 <emersonline2007@gmail.com>
2020-01-13 22:20:45 -0400

    - Criando Função de Pesquisa Termo

--- src/componentes/SearchBar.js
+++ src/componentes/SearchBar.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Segment, Input} from 'semantic-ui-react'
 
-const SearchBar = props => {
-    return (
-        <div className="search-bar">
-            <Segment stacked>
-                <Input icon="search" size="large" placeholder="search..."></Input>
-            </Segment>
-        </div>
-    )
+// const SearchBar = props =>{
+class SearchBar extends Component {
+
+    pesquisaTermo = e => {
+        if(e.keyCode === 13){
+            const termo = e.target.value
+            console.log(termo)
+        }
+    }
+
+    render(){
+        return (
+            <div className="search-bar">
+                <Segment stacked>
+                    <Input icon="search" onKeyDown={(e) => this.pesquisaTermo(e)} size="large" placeholder="search..."></Input>
+                </Segment>
+            </div>
+        )
+    }
 }
 
 export default SearchBar
